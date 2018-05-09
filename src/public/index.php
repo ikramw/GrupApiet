f546acdbfcfdecbcd1189d3a2e33aabc290666d9
ikramw <ikramw@hotmail.com>
2018-05-09 10:23:21 +0200

    initialise

--- src/public/index.php
+++ src/public/index.php
@@ -75,49 +75,5 @@ $app->get('/logout', function ($request, $response, $args) {
  * This was done so that we can check if the user is authed when calling '/api'
  * but we don't have to check for auth when calling '/signin'
  */
-$app->group('/api', function () use ($app) {
-
-    // GET http://localhost:XXXX/api/todos
-    $app->get('/todos', function ($request, $response, $args) {
-        /**
-         * $this->get('Todos') is available to us because we injected it into the container
-         * in 'App/container.php'. This makes it easier for us to call the database
-         * inside our routes.
-         */
-        //$this === $app
-        $allTodos = $this->get('Todos')->getAll();
-        /**
-         * Wrapping the data when returning as a safety thing
-         * https://www.owasp.org/index.php/AJAX_Security_Cheat_Sheet#Server_Side
-         */
-        return $response->withJson(['data' => $allTodos]);
-    });
-
-    // GET http://localhost:XXXX/api/todos/5
-    $app->get('/todos/{id}', function ($request, $response, $args) {
-        /**
-         * {id} is a placeholder for whatever you write after todos. So if we write
-         * /todos/4 the {id} will be 4. This gets saved in the $args array
-         * $args['id'] === 4
-         * The name inside of '$args' must match the placeholder in the url
-         * https://www.slimframework.com/docs/v3/objects/router.html#route-placeholders
-         */
-        $id = $args['id'];
-        $singleTodo = $this->todos->getOne($id);
-        return $response->withJson(['data' => $singleTodo]);
-    });
-
-    // POST http://localhost:XXXX/api/todos
-    $app->post('/todos', function ($request, $response, $args) {
-        /**
-         * Everything sent in 'body' when doing a POST-request can be
-         * extracted with 'getParsedBody()' from the request-object
-         * https://www.slimframework.com/docs/v3/objects/request.html#the-request-body
-         */
-        $body = $request->getParsedBody();
-        $newTodo = $this->todos->add($body);
-        return $response->withJson(['data' => $newTodo]);
-    });
-});
 
 $app->run();
